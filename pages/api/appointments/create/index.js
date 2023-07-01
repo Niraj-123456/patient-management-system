dc35d7ab0b75dee1bb21f65073ad60b915bc7faf
Niraj Lama <65294018+Niraj-123456@users.noreply.github.com>
2023-07-01 21:51:32 +0545

    remove edge function

--- pages/api/appointments/create/index.js
+++ pages/api/appointments/create/index.js
@@ -2,10 +2,6 @@ import prisma from "../../../../lib/prisma";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../auth/[...nextauth]";
 
-export const config = {
-  runtime: "edge",
-};
-
 export default async function handle(req, res) {
   const { hospital, department, appointmentDate, appointmentTime } = req.body;
   const session = await getServerSession(req, res, authOptions);
